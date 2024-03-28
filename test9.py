b5930c94090e1f95e8b32bb5ef2c50437b76c270
risqi <risqiikhsani16@gmail.com>
2024-03-28 18:49:44 +0700

    added test 9

---
+++ test9.py
@@ -0,0 +1,31 @@
+### Polynomial Regression
+
+# import matplotlib.pyplot as plt
+
+# x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+# y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+# plt.scatter(x, y)
+# plt.show() 
+
+
+import numpy
+import matplotlib.pyplot as plt
+
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+#NumPy has a method that lets us make a polynomial model
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+
+#Then specify how the line will display, we start at position 1, and end at position 22
+myline = numpy.linspace(1, 22, 100)
+
+#Draw the original scatter plot
+plt.scatter(x, y)
+
+#Draw the line of polynomial regression
+plt.plot(myline, mymodel(myline))
+
+#Display the diagram
+plt.show() 
