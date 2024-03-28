370cd4ba941ad032b3d95102382de42c8ef9e99a
risqi <risqiikhsani16@gmail.com>
2024-03-28 19:14:47 +0700

    added test10

--- test10.py
+++ test10.py
@@ -13,7 +13,16 @@ y = df['CO2']
 regr = linear_model.LinearRegression()
 regr.fit(X, y)
 
+# optional
+# The coefficient is a factor that describes the relationship with an unknown variable.
+# Example: if x is a variable, then 2x is x two times. x is the unknown variable, and the number 2 is the coefficient.
+# In this case, we can ask for the coefficient value of weight against CO2, and for volume against CO2. The answer(s) we get tells us what would happen if we increase, or decrease, one of the independent values.
+print(regr.coef_)
+
 #predict the CO2 emission of a car where the weight is 2300kg, and the volume is 1300cm3:
 predictedCO2 = regr.predict([[2300, 1300]])
 
 print(predictedCO2) # We have predicted that a car with 1.3 liter engine, and a weight of 2300 kg, will release approximately 107 grams of CO2 for every kilometer it drives.
+
+
+
